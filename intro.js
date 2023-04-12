e5698d0bb13781b96f5d048c757e25b785943ee5
estherniyo <estherniyonkuru123@gmail.com>
2023-04-12 17:50:33 +0200

    updated the read me file and also started with the easy rgex searches

---
+++ intro.js
@@ -0,0 +1,8 @@
+// the below line means that it will match what is in the brackets regarding the order they are in
+const reg = /[abc]/gm;
+
+// the below line means that if one has written a or b or c then followed by b it will match
+// Note the or in the above explanation
+const regex = /[abc][b]/gm;
+
+
